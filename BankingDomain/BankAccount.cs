3a64a757fdcd8156da3a1c6e497ff128a07b5f53
brandonj17 <37629186+brandonj17@users.noreply.github.com>
2021-03-17 15:14:06 -0400

    Initial commit

---
+++ BankingDomain/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BankingDomain
+{
+    public class BankAccount
+    {
+        private decimal _balance = 5000; // Class Variable "Field"
+        public decimal GetBalance()
+        {
+            return _balance;
+        }
+
+        public void Deposit(decimal amountToDeposit)
+        {
+            _balance += amountToDeposit;
+        }
+
+        public void Withdraw(decimal amountToWithdraw)
+        {
+            _balance -= amountToWithdraw;
+        }
+    }
+}
---
+++ BankingTests/BankAccountTests.cs
@@ -0,0 +1,58 @@
+﻿
+using BankingDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BankingTests
+{
+    public class BankAccountTests
+    {
+        private BankAccount _account;
+        private decimal _balance;
+
+        public BankAccountTests()
+        {
+            _account = new BankAccount();
+            _balance = _account.GetBalance();
+        }
+        [Fact]
+        public void NewAccountsHaveCorrectBalance()
+        {
+          
+            Assert.Equal(5000M, _balance);
+        }
+
+        [Fact]
+        public void DepositsIncreaseTheBalance()
+        {
+            // WTCYWYH
+            // Given
+          
+            var amountToDeposit = 42M;
+            // When
+            _account.Deposit(amountToDeposit);
+            // Then
+            Assert.Equal(
+                _balance + amountToDeposit,
+                _account.GetBalance()
+                );
+        }
+
+        [Fact]
+        public void WithdrawalsDecreaseTheBalance()
+        {
+       
+            var amountToWithdraw = 42M;
+
+            _account.Withdraw(amountToWithdraw);
+
+            Assert.Equal(
+                _balance - amountToWithdraw,
+                _account.GetBalance());
+        }
+    }
+}
